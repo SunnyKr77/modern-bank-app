054b81570aa279abbf7f7542296e8eb1995d483d
SunnyKr77 <sunnykrshah201@gmail.com>
2023-05-08 19:36:20 +0530

    project complete

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () =>  (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden" /> in few easy steps.</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Quickly discover the ideal card deal for you with minimal effort. Our intuitive platform guides you through a seamless process, making it effortless to compare and find better card offers. 
+      </p>
+      <Button styles="mt-10" />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-full h-full" />
+    </div>
+
+  </section>
+)
 
 export default CardDeal
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { feedback } from "../constants";
+import styles from "../style";
+import FeedbackCard from './FeedbackCard';
 
-const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
-  )
-}
+const Testimonials = () => (
+  <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}> 
+  {/* TODO */}
+    <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient" />
+
+    <section className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+      <h2 className={styles.heading2}>What people are<br className="sm:block hidden" /> saying about us</h2>
+
+      <div className="w-full md:mt-0 mt-6 ">
+        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+        Hear it straight from our satisfied customers. Discover what people are saying about us and our exceptional services.
+        </p>
+      </div>
+    </section>
+
+    <div className="flex flex-wrap justify-center w-full feedback-container relative z-[1]">
+      {feedback.map((card) => (
+        <FeedbackCard key={card.id} {...card} />
+      ))}
+
+    </div>
+  </section>
+)
 
 export default Testimonials
